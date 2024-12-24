c82256955c42d3952f4e594e6c40336c115133d1
SITHUWA11 <Wadagaming14@gmail.com>
2024-12-24 08:03:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "OyAxxbIR#7GxrnUvgRpNgcvj6sznfIt9BI8J_hcYYxqGUlU__0Ck",
+    SESSION_ID: process.env.SESSION_ID || "DZxEXJ4A#rZ1vM4ne7dVCzB7StxMpZdHEztN7nEE1cfUZYkqh0xM",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
